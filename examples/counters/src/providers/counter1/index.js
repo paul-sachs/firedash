5b633ad762e3b80430d4c597870d911ddfcdfe2e
Paul Sachs <paul.sachs@influitive.com>
2016-04-19 13:47:54 -0400

    Added basic persistance to counters.

--- examples/counters/src/providers/counter1/index.js
+++ examples/counters/src/providers/counter1/index.js
@@ -1,4 +1,12 @@
 import reducers from './reducers';
 import { actions, actionTypes } from './actions';
+import localforage from 'redux-replicate-localforage';
 
-export default { ...actionTypes, actions, reducers };
+// Replication options can allow easy optimistic updates and state
+// persistance. This example uses localstorage to save changes.
+
+const replication = {
+  replicator: localforage
+};
+
+export default { ...actionTypes, actions, reducers, replication };
