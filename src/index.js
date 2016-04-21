3f2c5832d8e2eeb3cba806e5dd42ce81b70b2316
Paul Sachs <paul.sachs@influitive.com>
2016-04-21 15:15:54 -0400

    Updated deps and added devtools.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import 'babel-polyfill';
 
 import React from 'react';
 import { render } from 'react-dom';
+// import connectDevtools from './common/devtools';
 
 import { App } from './components';
 
-render(<App />, document.getElementById('COMMUNITY_FEED_CONTAINER'));
+render(<App />, document.getElementById('app'));
 
 if (process.env.NODE_ENV !== 'production') {
   if (module.hot) {
